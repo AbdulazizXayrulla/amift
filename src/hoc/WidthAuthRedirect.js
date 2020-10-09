1e3f9e79da584135e7c5fca9f8d4f27404e8be95
XayrullayevAbdulaziz <64899975+XayrullayevAbdulaziz@users.noreply.github.com>
2020-10-09 21:48:48 +0400

    hoc1

--- src/hoc/WidthAuthRedirect.js
+++ src/hoc/WidthAuthRedirect.js
@@ -3,6 +3,7 @@ import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 
 let mapStateToPropsForRedirect=(state)=>{
+    
     return({
         isAuth:state.auth.isAuth
     })
