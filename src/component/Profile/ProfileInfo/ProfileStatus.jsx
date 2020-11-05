9e9ef638b6a3d9de51486363abc95b0e9b60b72c
XayrullayevAbdulaziz <64899975+XayrullayevAbdulaziz@users.noreply.github.com>
2020-11-05 11:44:06 +0500

    companentaDidUpdate

--- src/component/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/component/Profile/ProfileInfo/ProfileStatus.jsx
@@ -18,29 +18,38 @@ class ProfileStatus extends React.Component {
             editMode: false,
 
         })
+
         this.props.updateStatus(this.state.status)
 
     }
     changeStatus = (e) => {
-        console.log(e)
+
         this.setState({
             status: e.currentTarget.value
         })
 
     }
-
+componentDidUpdate(prevProps, prevState) {
+        if(prevProps.status!=this.props.status){
+            this.setState({
+                status:this.props.status
+            })
+        }
+}
 
     render() {
 
 
         return (
             <>
-                {!this.state.editMode ? <div>
+                {!this.state.editMode &&<div>
                     <span onDoubleClick={this.activEditMode}>{this.props.status ||"----"}</span>
-                </div> : < div>
+                </div> }
+                {
+                this.state.editMode &&< div>
                     <input onChange={this.changeStatus} autoFocus={true} onBlur={this.deactivEditMode.bind(this)} value={this.state.status}/>
                 </div>
-                }
+                }{console.log(this)}
 
 
             </>
